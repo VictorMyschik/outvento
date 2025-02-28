9b3f9f688ab357221a882671056a42dbdba5d5e6
Viktar Myshchyk <v.myshchik@apicworld.com>
2025-02-28 13:02:59 +0100

    Countries

--- database/migrations/2021_10_25_000500_create_country_table.php
+++ database/migrations/2021_10_25_000500_create_country_table.php
@@ -9,15 +9,13 @@ return new class extends Migration {
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
+            $table->string('name_ru', 50);
             $table->string('name_en', 50)->nullable();
             $table->string('name_pl', 50)->nullable();
             $table->char('iso3166alpha2', 3);
             $table->char('iso3166alpha3', 4);
             $table->char('iso3166numeric', 3);
             $table->tinyInteger('continent');
-
-            $table->timestamps();
         });
     }
 
