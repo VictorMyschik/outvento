e063c70609af779163cbf3cbfb32af5448e3e149
Viktar Myshchyk <v.myshchik@apicworld.com>
2025-03-04 11:50:30 +0100

    Reference images

--- app/Console/Commands/purge_data/travel_types.sql
+++ app/Console/Commands/purge_data/travel_types.sql
@@ -1,5 +1,5 @@
 insert into public.travel_types (id, name_ru, name_en, name_pl, image_path)
-values  (2, 'Горный поход', 'Mountain campaign', 'Kampania górska', null),
-        (3, 'Хайкинг', 'Hyking', 'Hyking', null),
-        (4, 'Байдарки', 'Kayaks', 'Kajaki', null),
-        (1, 'Велопоход', 'Cycling', 'Kolarstwo', '/images/reference/travel_type/4040393747.png');
+values  (2, 'Горный поход', 'Mountain campaign', 'Kampania górska', '/images/reference/travel_type/1911113805.png'),
+        (4, 'Байдарки', 'Kayaks', 'Kajaki', '/images/reference/travel_type/821383342.png'),
+        (3, 'Хайкинг', 'Hyking', 'Hyking', '/images/reference/travel_type/3998012191.png'),
+        (1, 'Велопоход', 'Cycling', 'Kolarstwo', '/images/reference/travel_type/421226735.png');
