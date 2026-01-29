026dcc7c4bd0235f1d367aa81d7d390c1f9f735f
Allximik <allximik50@gmail.com>
2026-01-29 11:24:26 +0100

    n/s

--- lang/en/common.php
+++ lang/en/common.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'slogan'       => 'Welcome',
-    'newsletter'   => 'Newsletter',
-    'published_at' => 'Published',
-    'draft'        => 'draft',
-    'active'       => 'active',
-    'archived'     => 'archived',
+    'slogan'              => 'Welcome',
+    'newsletter'          => 'Newsletter',
+    'published_at'        => 'Published',
+    'draft'               => 'draft',
+    'active'              => 'active',
+    'archived'            => 'archived',
+    'reset_password_page' => 'Reset Password Page',
 
     'legal_documents' => [
         'terms'   => 'Terms and Conditions',
--- lang/pl/common.php
+++ lang/pl/common.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'slogan'       => 'Welcome',
-    'newsletter'   => 'Newsletter',
-    'published_at' => 'Opublikowano',
-    'draft'        => 'szkic',
-    'active'       => 'aktywny',
-    'archived'     => 'zarchiwizowany',
+    'slogan'              => 'Welcome',
+    'newsletter'          => 'Newsletter',
+    'published_at'        => 'Opublikowano',
+    'draft'               => 'szkic',
+    'active'              => 'aktywny',
+    'archived'            => 'zarchiwizowany',
+    'reset_password_page' => 'Strona resetowania hasła',
 
     'legal_documents' => [
         'terms'   => 'Regulamin',
